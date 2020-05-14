f85b14861a33505ed3613357fac1c01c97765c11
Vadim Smilgin <vadimsmilgin64@gmail.com>
2020-05-14 17:00:12 +0300

    Added dictionary that contains SObjest and related PBs
    Signed-off-by: Vadim Smilgin <vadimsmilgin64@gmail.com>

--- PackageCreator.py
+++ PackageCreator.py
@@ -14,6 +14,10 @@ folder_name_workflows = 'workflows'
 
 generalFolder = os.getcwd()
 
+regexp_find_s_object_name = '(?<=<name>ObjectType</name>\n\s{8}<value>\n\s{12}<stringValue>).*?(?=</stringValue>)'
+regexp_find_s_object_name_from_file_name = '.*?(?=\.)'
+map_s_objects_and_pb = {}
+
 
 def get_sobject_name_from_pbs_and_workflows():
     try:
@@ -23,22 +27,22 @@ def get_sobject_name_from_pbs_and_workflows():
             flows_dir = os.listdir(path=flows_path)
             if len(flows_dir) > 0:
                 os.chdir(flows_path)
-                for file in flows_dir:
-                    with open(file, "r") as fh:
-                        sobject_name = re.search(
-                            r'(?<=<name>ObjectType</name>\n\s{8}<value>\n\s{12}<stringValue>).*?(?=</stringValue>)',
-                            fh.read()
-                        )
-                        if sobject_name is not None:
-                            set_sobjects.add(sobject_name.group(0))
+                for flow in flows_dir:
+                    with open(flow, "r") as file:
+                        s_object_name = re.search(regexp_find_s_object_name, file.read())
+                        if s_object_name is not None:
+                            set_sobjects.add(s_object_name.group(0))
+                            pb_name = re.search(regexp_find_s_object_name_from_file_name, flow).group(0)
+                            set_key(map_s_objects_and_pb, s_object_name.group(0), pb_name)
             os.chdir(generalFolder)
         if folder_name_workflows in automations_dir:
             workflows_dir = os.listdir(path=workflows_path)
             if len(workflows_dir) > 0:
                 for fileName in workflows_dir:
-                    sObjectName = re.search(r'.*?(?=\.)', fileName).group(0)
-                    set_sobjects.add(sObjectName)
+                    s_object_name = re.search(regexp_find_s_object_name_from_file_name, fileName).group(0)
+                    set_sobjects.add(s_object_name)
         create_package_xml(set_sobjects)
+        print(map_s_objects_and_pb)
     except FileNotFoundError:
         print('Directory /sample/retrieveUnpackaged is not found')
 
@@ -78,12 +82,14 @@ def create_package_xml(*args):
         package.write(result)
 
 
+def set_key(dictionary, key, value):
+    if key not in dictionary:
+        dictionary[key] = value
+    elif type(dictionary[key]) == list:
+        dictionary[key].append(value)
+    else:
+        dictionary[key] = [dictionary[key], value]
+
+
 if __name__ == "__main__":
     get_sobject_name_from_pbs_and_workflows()
-
-
-
-
-
-
-
