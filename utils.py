46433ad328f7529c115289d4d8229f9337a3992d
Vadim Smilgin <vadimsmilgin64@gmail.com>
2020-05-20 15:31:57 +0300

    Added some utils functions
    Signed-off-by: Vadim Smilgin <vadimsmilgin64@gmail.com>

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import re
 
 rootFolder = os.getcwd()
 automation_files_path = '.\\sample\\retrieveUnpackaged'
@@ -16,7 +17,6 @@ folder_name_workflows = 'workflows'
 regexp_find_s_object_name = '(?<=<name>ObjectType</name>\n\s{8}<value>\n\s{12}<stringValue>).*?(?=</stringValue>)'
 regexp_find_s_object_name_from_file_name = '.*?(?=\.)'
 regexp_find_lookup_fields = '(?<=<fullName>)([a-zA-Z0-9_]+)(?:(?!<fields>)(?!</fields>).)*?(?=<type>Lookup</type>)'
-regexp_find_s_object_name_from_file_name = '.*?(?=\.)'
 regexp_find_pb_vulnerable_conditionals = '<\/leftValueReference>\n\s{16}<operator>IsNull<\/operator>' + \
                                          '\n\s{16}<rightValue>\n\s{20}<booleanValue>true'
 regexp_find_wf_vulnerable_conditionals_1 = '<rules>\n\s{8}<fullName>(.*?)</fullName>.*?<formula>.*?ISNULL.*?'
@@ -33,3 +33,22 @@ def set_key(dictionary, key, value):
 def get_str_for_regexp(lookup_fields_list):
     sepatator = '|'
     return '(' + sepatator.join(lookup_fields_list) + ')'
+
+
+def add_dict_to_list(current_list, dictionary):
+    if bool(dictionary) is True:
+        current_list.append(dictionary)
+
+
+def get_pb_result(dictionary, name, errors):
+    dictionary['name'] = re.search(regexp_find_s_object_name_from_file_name, name).group(0)
+    dictionary['lookup_fields'] = errors
+
+
+def get_wf_result(dictionary, name, errors):
+    dictionary['sObject'] = re.search(regexp_find_s_object_name_from_file_name, name).group(0)
+    error_list = []
+    for error in errors:
+        dict_error = {'name': error[0], 'lookup_field': error[1]}
+        error_list.append(dict_error)
+    dictionary['workflows'] = error_list
