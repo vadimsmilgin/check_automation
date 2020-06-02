b53cad5e23e7b2002c49d3b7083f7683158d840e
Vadim Smilgin <vadimsmilgin64@gmail.com>
2020-06-02 12:49:26 +0300

    Added a path to python.exe
    Signed-off-by: Vadim Smilgin <vadimsmilgin64@gmail.com>

--- context.py
+++ context.py
@@ -39,24 +39,27 @@ class Context:
     def get_retrieve_sobjects_path(self):
         return self._strategy.get_retrieve_sobjects_path()
 
+    def get_python_exe_path(self):
+        return self._strategy.get_python_exe_path()
+
     def execute(self):
         os.chdir(self._strategy.get_sample_path())
         output = subprocess.run(command, shell=True)
         if output.returncode == 0:
             os.chdir(utils.rootFolder)
-            package_creator = subprocess.run([sys.executable, 's_object_package_creator.py'], check=True)
+            package_creator = subprocess.run([self.get_python_exe_path(), 's_object_package_creator.py'], check=True)
             if package_creator.returncode == 0:
                 os.chdir(self._strategy.get_sample_path())
                 output2 = subprocess.run(command2, shell=True)
                 if output2.returncode == 0:
                     os.chdir(utils.rootFolder)
-                    wf_package_creator = subprocess.run([sys.executable, 'workflow_package_creator.py'], check=True)
+                    wf_package_creator = subprocess.run([self.get_python_exe_path(), 'workflow_package_creator.py'], check=True)
                     if wf_package_creator.returncode == 0:
                         os.chdir(self._strategy.get_sample_path())
                         output3 = subprocess.run(command3, shell=True)
                         if output3.returncode == 0:
                             os.chdir(utils.rootFolder)
-                            error_validator = subprocess.run([sys.executable, 'error_validator.py'], check=True, stdout=subprocess.PIPE)
+                            error_validator = subprocess.run([self.get_python_exe_path(), 'error_validator.py'], check=True, stdout=subprocess.PIPE)
                             print(error_validator.stdout.decode('utf-8'))
 
 
@@ -85,6 +88,10 @@ class Strategy(ABC):
     def get_s_objects_path(self):
         pass
 
+    @abstractmethod
+    def get_python_exe_path(self):
+        pass
+
 
 class Windows(Strategy):
     sample_path = '.\\sample'
@@ -94,6 +101,7 @@ class Windows(Strategy):
     workflows_path = '.\\sample\\retrieveUnpackaged\\workflows'
     retrieve_sobjects_path = '.\\sample\\retrieveSObjects'
     s_objects_path = '.\\sample\\retrieveSObjects\\objects'
+    python_exe_path = '.\\venv\\Scripts\\python.exe'
 
     def get_sample_path(self):
         return Windows.sample_path
@@ -113,6 +121,9 @@ class Windows(Strategy):
     def get_s_objects_path(self):
         return Windows.s_objects_path
 
+    def get_python_exe_path(self):
+        return Windows.python_exe_path
+
 
 class MacOS(Strategy):
     sample_path = './sample'
@@ -122,6 +133,7 @@ class MacOS(Strategy):
     workflows_path = './sample/retrieveUnpackaged/workflows'
     s_objects_path = './sample/retrieveSObjects/objects'
     retrieve_sobjects_path = './sample/retrieveSObjects'
+    python_exe_path = './venv/Scripts/python.exe'
 
     def get_sample_path(self):
         return MacOS.sample_path
@@ -140,6 +152,9 @@ class MacOS(Strategy):
 
     def get_s_objects_path(self):
         return MacOS.s_objects_path
+
+    def get_python_exe_path(self):
+        return MacOS.python_exe_path
 
 
 # class Linux(Strategy):
