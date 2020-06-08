13456d15ff52eab4ca7d53e93a1600d9c7696dc7
Vadim Smilgin <vadimsmilgin64@gmail.com>
2020-06-08 10:03:53 +0300

    Added a check for a necessity lib, if the lib doesn't exist, it'll be installed.
    Signed-off-by: Vadim Smilgin <vadimsmilgin64@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
 
+import sys
+import subprocess
+import pkg_resources
 import platform
 from context import Context, Windows, MacOS
 
+required = {'lxml'}
+
 
 def start():
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
+
+    if missing:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+
     if platform.system() == 'Windows':
         context1 = Context(Windows())
     if platform.system() == 'Darwin':
